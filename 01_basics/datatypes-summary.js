6b88488c49e386dd3d7f72dfff1cdc3a425c7d59
TanmayMalpure <120240794+TanmayMalpure@users.noreply.github.com>
2025-11-24 09:48:54 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+//  # Primitive 
+
+// 7 types: String ,Number , Boolean, null , undefind, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// const bigNumber = 12345678956451n
+
+
+// Reference (Non primitive)
+
+//Array, objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Tanmay",
+    age: 21,
+    email: "tanmaymalpure9@gmail.com"
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof anotherId);
+
+
