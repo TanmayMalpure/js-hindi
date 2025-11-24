e2f6c2051a2b24167312d17cade73dd62a96efba
TanmayMalpure <120240794+TanmayMalpure@users.noreply.github.com>
2025-11-24 09:15:34 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,11 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
 // console.log("2" > 1);
 // console.log("02" > 1);
-
 
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
-
 
 console.log(undefined == 0);
 console.log(undefined > 0);
@@ -13,6 +18,4 @@ console.log(undefined < 0);
 
 // === 
 
-console.log("2" === 2);
-
-
+console.log(2 ==="2");
